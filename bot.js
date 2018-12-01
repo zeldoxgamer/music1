ce99e59623511442a48aa924c387b7857d771cea
zeldoxgamer <45310372+zeldoxgamer@users.noreply.github.com>
2018-12-01 14:32:42 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`1play || +1help`,"http://twitch.tv/S-F")
+client.user.setGame(`+1play || +1help`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
